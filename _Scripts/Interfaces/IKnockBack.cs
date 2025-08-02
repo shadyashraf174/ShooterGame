cb6ba426fb9c5377018b2e6ed4b888cb46734b6c
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-08-02 12:46:00 +0700

    new

--- _Scripts/Interfaces/IKnockBack.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IKnockBack
-{
-    void KnockBack(Vector2 direction, float power, float duration);
-}
